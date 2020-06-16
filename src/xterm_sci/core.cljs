47e3bcf1f450761c14a0b9e98df141427c272d42
Arne Brasseur <arne@arnebrasseur.net>
2020-06-16 17:46:20 +0200

    Add local-echo, much better this way!

--- src/xterm_sci/core.cljs
+++ src/xterm_sci/core.cljs
@@ -1,6 +1,6 @@
 (ns xterm-sci.core
   (:require ["xterm" :as xterm]
-            #_["local-echo" :as local-echo]
+            ["local-echo" :as local-echo :default local-echo-controller]
             [clojure.tools.reader.reader-types :as r]
             [clojure.string :as str]
             [sci.core :as sci]
@@ -10,20 +10,22 @@
             [sci.impl.utils :as utils]
             [goog.crypt :as c]))
 
-;; causing compilation issues
-#_local-echo/LocalEchoController
+
+
 
 
 (defn string->byte-seq [s]
   (c/stringToUtf8ByteArray s))
 
 (defonce term (doto (xterm/Terminal.)
-                (.open (js/document.getElementById "app"))
-                (.write "user=> ")))
+                (.open (js/document.getElementById "app"))))
+
+(defonce line-discipline (local-echo-controller. term))
 
 (def unconsumed-input (atom ""))
 (def env (atom {}))
-(def ctx (sci/init {:env env}))
+(def ctx (sci/init {:env env
+                    :classes {'js js/window}}))
 
 (defn read-form [s]
   (try
@@ -45,28 +47,44 @@
       (let [form (read-form @unconsumed-input)]
         (if (= ::eof form)
           (when-not (= ::none ret)
-            (.write term (str (pr-str ret) "\r\n" @vars/current-ns "=> ")))
+            (.write term (str (pr-str ret) "\r\n" )))
           (let [[expr remainder] form
-                ret (sci-impl/eval-form ctx expr)]
+                ret (try
+                      (sci-impl/eval-form ctx expr)
+                      (catch :default e
+                        (swap! env assoc-in [:namespaces (vars/current-ns-name) '*e] (vars/SciVar. e '*e {}))
+                        (.write term (pr-str e))
+                        (.write term "\r\n")
+                        ::none))]
             (reset! unconsumed-input remainder)
             (recur ret)))))))
 
+(get-in @env '[:namespaces user])
+;; (defonce handler
+;;   (.onData term (fn [data]
+;;                   (prn (string->byte-seq data))
+;;                   (if (= (char 127) data)
+;;                     (swap! unconsumed-input
+;;                            (fn [input]
+;;                              (let [lines (str/split input #"\r")]
+;;                                (if (seq (last lines))
+;;                                  (do
+;;                                    (.write term "\u001b[1D \u001b[1D")
+;;                                    (subs input 0 (dec (count input))))
+;;                                  input))))
+;;                     (do
+;;                       (swap! unconsumed-input + data)
+;;                       (.write term data)
+;;                       (when (str/includes? data "\r")
+;;                         (.write term "\r\n")
+;;                         (try-eval!)))))))
 
-(defonce handler
-  (.onData term (fn [data]
-                  (prn (string->byte-seq data))
-                  (if (= (char 127) data)
-                    (swap! unconsumed-input
-                           (fn [input]
-                             (let [lines (str/split input #"\r")]
-                               (if (seq (last lines))
-                                 (do
-                                   (.write term "\u001b[1D \u001b[1D")
-                                   (subs input 0 (dec (count input))))
-                                 input))))
-                    (do
-                      (swap! unconsumed-input + data)
-                      (.write term data)
-                      (when (str/includes? data "\r")
-                        (.write term "\r\n")
-                        (try-eval!)))))))
+(defn input-loop []
+  (.then (.read line-discipline (str @vars/current-ns "=> "))
+         (fn [line]
+           (swap! unconsumed-input + line)
+           (try-eval!)
+           (input-loop))))
+
+(defonce i
+  (input-loop))
